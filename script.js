85d6622bb523762f499fcec0321e93b0ae1ba28e
Терленев Дмитрий <terlenev.dk@office.volga-soft.ru>
2025-06-26 17:51:16 +0300

    swipers

--- script.js
+++ script.js
@@ -13,4 +13,44 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         slidesPerView: 1,
     });
+
+    const performanceManagementSlider = new Swiper("#performance-management", {
+        spaceBetween: 18,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.performance-management-next',
+            prevEl: '.performance-management-prev',
+        },
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+            },
+            865: {
+                slidesPerView: 'auto',
+                allowTouchMove: false,
+            },
+        }
+    });
+
+    const talentManagementSlider = new Swiper("#talent-management", {
+        spaceBetween: 18,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.talent-management-next',
+            prevEl: '.talent-management-prev',
+        },
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+            },
+            865: {
+                slidesPerView: 'auto',
+                allowTouchMove: false,
+            },
+        }
+    });
 });

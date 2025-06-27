e3c592c73f79aa7c7639a6559aaa1184a611b700
Терленев Дмитрий <terlenev.dk@office.volga-soft.ru>
2025-06-27 16:43:20 +0300

    adaptive

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 1,
     });
 
+    // Key Effects Sliders
+
+    const automaticSelectionSlider = new Swiper("#automatic-selection", {
+        spaceBetween: 18,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.automatic-selection-next',
+            prevEl: '.automatic-selection-prev',
+        },
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+            },
+            865: {
+                slidesPerView: 'auto',
+                allowTouchMove: false,
+            },
+        }
+    });
+
     const performanceManagementSlider = new Swiper("#performance-management", {
         spaceBetween: 18,
         slidesPerView: 'auto',
@@ -41,6 +63,46 @@ document.addEventListener('DOMContentLoaded', () => {
         navigation: {
             nextEl: '.talent-management-next',
             prevEl: '.talent-management-prev',
+        },
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+            },
+            865: {
+                slidesPerView: 'auto',
+                allowTouchMove: false,
+            },
+        }
+    });
+
+    const LKkedoSlider = new Swiper("#LK-kedo", {
+        spaceBetween: 18,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.LK-kedo-next',
+            prevEl: '.LK-kedo-prev',
+        },
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+            },
+            865: {
+                slidesPerView: 'auto',
+                allowTouchMove: false,
+            },
+        }
+    });
+
+    const cafeteryOfBenefitsSlider = new Swiper("#cafetery-of-benefits", {
+        spaceBetween: 18,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.cafetery-of-benefits-next',
+            prevEl: '.cafetery-of-benefits-prev',
         },
         breakpoints: {
             10: {
